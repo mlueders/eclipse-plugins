29152c625d1530990133eac278f017c6ec803da7
mlueders <mike@roadkill.org>
2011-03-27 00:48:51 -0500

    fixed transition logic

--- devtools/src/com/pfs/devtools/actions/ActivateEditorAction.java
+++ devtools/src/com/pfs/devtools/actions/ActivateEditorAction.java
@@ -4,8 +4,6 @@
  */
 package com.pfs.devtools.actions;
 
-import com.pfs.base.PluginSupport;
-
 import org.eclipse.jface.action.IAction;
 import org.eclipse.swt.custom.CTabFolder;
 import org.eclipse.swt.custom.CTabItem;
@@ -17,64 +15,82 @@ import org.eclipse.ui.internal.EditorSite;
 import org.eclipse.ui.internal.presentations.PresentablePart;
 import org.eclipse.ui.internal.presentations.defaultpresentation.DefaultTabItem;
 
+import com.pfs.base.PluginSupport;
+
 /**
  * Possibly the biggest hack I've ever laid down, but those fuckers left me no choice when they made the default behavior of navigating
  * beyond the current set of tabs that fucking pop-up editor list and NO WAY TO CHANGE IT! Fuck!
  */
 public abstract class ActivateEditorAction extends DevToolsAction {
 	public static class Next extends ActivateEditorAction {
-		protected int getIndexToActivate(int selectedIndex, int selectedVisibleIndex, int[] visibleIndices, int lastVisibleIndex,
-				CTabItem[] items) throws Exception {
-			int indexToActivate;
-
-			if (selectedVisibleIndex < lastVisibleIndex) {
-				return visibleIndices[selectedVisibleIndex + 1];
-			} else {
-				indexToActivate = selectedIndex + 1;
-
-				if (indexToActivate >= visibleIndices.length) {
-					for (int i = 0; i < lastVisibleIndex + 1; i++) {
-						if (i >= items.length) {
-							continue;
-						}
-						getEditorPane(items[i]).setFocus();
-					}
-
-					indexToActivate = 0;
+		private int getMaxVisibleIndex(CTabItem[] items) {
+			for (int i = items.length - 1; i >= 0; i--) {
+				if (items[i].isShowing()) {
+					return i;
 				}
-				return indexToActivate;
 			}
+			return -1;
+		}
+		
+		protected void setFocusIfTransition(int indexToActivate, CTabItem[] items) throws Exception {
+			if (indexToActivate == 0) {
+				for (int i = indexToActivate; i < items.length; i++) {
+					getEditorPane(items[i]).setFocus();
+					int maxVisibleIndex = getMaxVisibleIndex(items);
+					
+					if (i == maxVisibleIndex) {
+						break;
+					}
+				}
+			}
+		}
+		
+		protected int getIndexToActivate(int selectedIndex, CTabItem[] items) throws Exception {
+			int indexToActivate = selectedIndex + 1;
+			
+			if (indexToActivate >= items.length) {
+				indexToActivate = 0;
+			}
+			return indexToActivate;
 		}
 	}
 
 	public static class Previous extends ActivateEditorAction {
-		protected int getIndexToActivate(int selectedIndex, int selectedVisibleIndex, int[] visibleIndices, int lastVisibleIndex,
-				CTabItem[] items) throws Exception {
-			int indexToActivate;
-			if (selectedVisibleIndex > 0) {
-				return visibleIndices[selectedVisibleIndex - 1];
-			} else {
-				indexToActivate = selectedIndex - 1;
-
-				// when we cross over from first to last index, need to make all necessary editors visible
-				if (indexToActivate < 0) {
-					for (int i = (visibleIndices.length - lastVisibleIndex - 1); i < visibleIndices.length; i++) {
-						if (i < 0) {
-							continue;
-						}
-						getEditorPane(items[i]).setFocus();
-					}
-
-					indexToActivate = visibleIndices.length - 1;
+		private int getMinVisibleIndex(CTabItem[] items) {
+			for (int i = 0; i < items.length; i++) {
+				if (items[i].isShowing()) {
+					return i;
 				}
-				return indexToActivate;
 			}
+			return -1;
+		}
+		
+		protected void setFocusIfTransition(int indexToActivate, CTabItem[] items) throws Exception {
+			if (indexToActivate == items.length - 1) {
+				for (int i = items.length - 1; i >= 0; i--) {
+					getEditorPane(items[i]).setFocus();
+					int minVisibleIndex = getMinVisibleIndex(items);
+					
+					if (i == minVisibleIndex) {
+						break;
+					}
+				}
+			}
+		}
+		
+		protected int getIndexToActivate(int selectedIndex, CTabItem[] items) throws Exception {
+			int indexToActivate = selectedIndex - 1;
+			
+			if (indexToActivate < 0) {
+				indexToActivate = items.length - 1;
+			}
+			return indexToActivate;
 		}
 	}
 
-	protected abstract int getIndexToActivate(int selectedIndex, int selectedVisibleIndex, int[] visibleIndeces, int lastVisibleIndex,
-			CTabItem[] items) throws Exception;
-
+	protected abstract int getIndexToActivate(int selectedIndex, CTabItem[] items) throws Exception;
+	protected abstract void setFocusIfTransition(int indexToActivate, CTabItem[] items) throws Exception;
+		
 	protected void onRun(IAction action) throws Exception {
 		IWorkbenchPage activePage = getWindow().getActivePage();
 		IEditorPart activeEditor = activePage.getActiveEditor();
@@ -88,7 +104,7 @@ public abstract class ActivateEditorAction extends DevToolsAction {
 				if (editorTitleToActivate.equals(editors[i].getTitle())) {
 					IEditorPart editorPartToActivate = editors[i].getEditor(true);
 					EditorSite site = (EditorSite) editorPartToActivate.getEditorSite();
-
+					
 					if (editorToActivate.equals(site.getPane())) {
 						activePage.activate(editorPartToActivate);
 						return;
@@ -97,40 +113,25 @@ public abstract class ActivateEditorAction extends DevToolsAction {
 			}
 		}
 	}
-
+	
 	private EditorPane getEditorToActivate(IEditorPart activeEditor) throws Exception {
 		EditorSite site = (EditorSite) activeEditor.getSite();
 		EditorPane pane = (EditorPane) site.getPane();
 
 		CTabFolder folder = (CTabFolder) pane.getWorkbook().getControl();
-		CTabItem[] items = folder.getItems();
 		int selectedIndex = folder.getSelectionIndex();
-
-		int[] visible = new int[items.length];
-		int selectedVisibleIndex = -1;
-		int lastVisibleIndex = -1;
-		for (int i = 0; i < items.length; i++) {
-			if (items[i].isShowing()) {
-				++lastVisibleIndex;
-
-				if (i == selectedIndex) {
-					selectedVisibleIndex = lastVisibleIndex;
-				}
-
-				visible[lastVisibleIndex] = i;
-			}
-		}
-
-		if (selectedVisibleIndex < 0) {
+		CTabItem[] items = folder.getItems();
+		
+		if (selectedIndex < 0) {
 			return null;
 		}
-
-		int indexToActivate = getIndexToActivate(selectedIndex, selectedVisibleIndex, visible, lastVisibleIndex, items);
+		
+		int indexToActivate = getIndexToActivate(selectedIndex, items);
+		setFocusIfTransition(indexToActivate, items);
 		CTabItem item = folder.getItem(indexToActivate);
-
 		return getEditorPane(item);
 	}
-
+	
 	protected EditorPane getEditorPane(CTabItem item) throws Exception {
 		DefaultTabItem tabItem = (DefaultTabItem) item.getData();
 		PresentablePart presentable = (PresentablePart) tabItem.getData();
