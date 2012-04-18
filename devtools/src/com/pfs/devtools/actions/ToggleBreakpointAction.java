f01ba4fa50bf34ce7dc6d9029f24fb906b752f60
Mike Lueders <mlueders@fountainhead.(none)>
2012-04-17 21:49:08 -0500

    updated for eclipse 3.7.x

--- devtools/src/com/pfs/devtools/actions/ToggleBreakpointAction.java
+++ devtools/src/com/pfs/devtools/actions/ToggleBreakpointAction.java
@@ -1,5 +1,6 @@
 package com.pfs.devtools.actions;
 
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.internal.debug.ui.actions.ToggleBreakpointAdapter;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
@@ -27,9 +28,9 @@ public class ToggleBreakpointAction extends DevToolsAction {
 		}
 	}
 
-	private void toggleBreakpoint(IEditorPart activeEditor, ISelection selection) {
+	private void toggleBreakpoint(IEditorPart activeEditor, ISelection selection) throws CoreException {
 		if (adapter.canToggleLineBreakpoints(activeEditor, selection)) {
-			adapter.toggleLineBreakpoints(activeEditor, selection, true);
+			adapter.toggleLineBreakpoints(activeEditor, selection);
 		} else if (adapter.canToggleWatchpoints(activeEditor, selection)) {
 			adapter.toggleWatchpoints(activeEditor, selection);
 		}
