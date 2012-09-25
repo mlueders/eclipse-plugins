167c71df52517663b1ce2bb085c9cc83c0caa38f
Mike Lueders <mike@roadkill.org>
2012-09-24 22:54:29 -0500

    updated to eclipse-4.2; added mac key bindings

--- devtools/src/com/pfs/devtools/actions/ActivateEditorAction.java
+++ devtools/src/com/pfs/devtools/actions/ActivateEditorAction.java
@@ -4,51 +4,106 @@
  */
 package com.pfs.devtools.actions;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.e4.ui.model.application.ui.MElementContainer;
+import org.eclipse.e4.ui.model.application.ui.MUIElement;
+import org.eclipse.e4.ui.model.application.ui.advanced.MPlaceholder;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.model.application.ui.basic.MPartStack;
+import org.eclipse.e4.ui.model.application.ui.basic.MStackElement;
 import org.eclipse.jface.action.IAction;
-import org.eclipse.swt.custom.CTabFolder;
-import org.eclipse.swt.custom.CTabItem;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IEditorReference;
 import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.internal.EditorPane;
-import org.eclipse.ui.internal.EditorSite;
-import org.eclipse.ui.internal.presentations.PresentablePart;
-import org.eclipse.ui.internal.presentations.defaultpresentation.DefaultTabItem;
-
-import com.pfs.base.PluginSupport;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.internal.WorkbenchPage;
 
 /**
  * Possibly the biggest hack I've ever laid down, but those fuckers left me no choice when they made the default behavior of navigating
  * beyond the current set of tabs that fucking pop-up editor list and NO WAY TO CHANGE IT! Fuck!
  */
 public abstract class ActivateEditorAction extends DevToolsAction {
-	public static class Next extends ActivateEditorAction {
-		private int getMaxVisibleIndex(CTabItem[] items) {
-			for (int i = items.length - 1; i >= 0; i--) {
-				if (items[i].isShowing()) {
+	
+	private static final class EditorManager {
+		
+		private IWorkbenchPage activePage;
+		private MPart[] editorParts;
+		
+		public EditorManager(IWorkbenchWindow window) {
+			activePage = window.getActivePage();
+			initEditorParts(window);
+		}
+		
+		public MPart[] getEditorParts() {
+			return editorParts;
+		}
+		
+		public IEditorReference getEditorReferenceForIndex(int index) {
+			if ((index >= 0) && (index < editorParts.length)) {
+				String editorTitle = editorParts[index].getLabel();
+				if (editorTitle != null) {
+					for (IEditorReference editorReference : activePage.getEditorReferences()) {
+						if (editorTitle.equals(editorReference.getTitle())) {
+							return editorReference;
+						}
+					}
+				}
+			}
+			return null;
+		}
+
+		public int getIndexForTitle(String title) {
+			if (title == null) {
+				return -1;
+			}
+			
+			for (int i = 0; i < editorParts.length; i++) {
+				if (title.equals(editorParts[i].getLabel())) {
 					return i;
 				}
 			}
 			return -1;
 		}
 		
-		protected void setFocusIfTransition(int indexToActivate, CTabItem[] items) throws Exception {
-			if (indexToActivate == 0) {
-				for (int i = indexToActivate; i < items.length; i++) {
-					getEditorPane(items[i]).setFocus();
-					int maxVisibleIndex = getMaxVisibleIndex(items);
-					
-					if (i == maxVisibleIndex) {
-						break;
+		private void initEditorParts(IWorkbenchWindow window) {
+			List<MPart> editorPartList = new ArrayList<MPart>();
+			WorkbenchPage page = (WorkbenchPage) window.getActivePage();
+			
+			if (page != null) {
+				MUIElement area = page.findSharedArea();
+				if (area instanceof MPlaceholder) {
+					area = ((MPlaceholder) area).getRef();
+				}
+
+				MPartStack activeStack = getActiveStack(area);
+				if (activeStack != null) {
+					for (MStackElement element : activeStack.getChildren()) {
+						editorPartList.add((MPart) element);
 					}
 				}
 			}
+			editorParts = editorPartList.toArray(new MPart[0]);
+		}
+
+		private MPartStack getActiveStack(Object element) {
+			if (element instanceof MPartStack) {
+				return (MPartStack) element;
+			} else if (element instanceof MElementContainer<?>) {
+				return getActiveStack(((MElementContainer<?>) element).getSelectedElement());
+			}
+			return null;
 		}
 		
-		protected int getIndexToActivate(int selectedIndex, CTabItem[] items) throws Exception {
+	}
+
+	
+	public static class Next extends ActivateEditorAction {
+		protected int getIndexToActivate(int selectedIndex, MPart[] editors) throws Exception {
 			int indexToActivate = selectedIndex + 1;
 			
-			if (indexToActivate >= items.length) {
+			if (indexToActivate >= editors.length) {
 				indexToActivate = 0;
 			}
 			return indexToActivate;
@@ -56,85 +111,33 @@ public abstract class ActivateEditorAction extends DevToolsAction {
 	}
 
 	public static class Previous extends ActivateEditorAction {
-		private int getMinVisibleIndex(CTabItem[] items) {
-			for (int i = 0; i < items.length; i++) {
-				if (items[i].isShowing()) {
-					return i;
-				}
-			}
-			return -1;
-		}
-		
-		protected void setFocusIfTransition(int indexToActivate, CTabItem[] items) throws Exception {
-			if (indexToActivate == items.length - 1) {
-				for (int i = items.length - 1; i >= 0; i--) {
-					getEditorPane(items[i]).setFocus();
-					int minVisibleIndex = getMinVisibleIndex(items);
-					
-					if (i == minVisibleIndex) {
-						break;
-					}
-				}
-			}
-		}
-		
-		protected int getIndexToActivate(int selectedIndex, CTabItem[] items) throws Exception {
+		protected int getIndexToActivate(int selectedIndex, MPart[] editors) throws Exception {
 			int indexToActivate = selectedIndex - 1;
 			
 			if (indexToActivate < 0) {
-				indexToActivate = items.length - 1;
+				indexToActivate = editors.length - 1;
 			}
 			return indexToActivate;
 		}
 	}
 
-	protected abstract int getIndexToActivate(int selectedIndex, CTabItem[] items) throws Exception;
-	protected abstract void setFocusIfTransition(int indexToActivate, CTabItem[] items) throws Exception;
+	protected abstract int getIndexToActivate(int selectedIndex, MPart[] editors) throws Exception;
 		
 	protected void onRun(IAction action) throws Exception {
 		IWorkbenchPage activePage = getWindow().getActivePage();
 		IEditorPart activeEditor = activePage.getActiveEditor();
-
+		
 		if (activeEditor != null) {
-			EditorPane editorToActivate = getEditorToActivate(activeEditor);
-			String editorTitleToActivate = editorToActivate.getPartReference().getTitle();
-			IEditorReference[] editors = activePage.getEditorReferences();
-
-			for (int i = 0; i < editors.length; i++) {
-				if (editorTitleToActivate.equals(editors[i].getTitle())) {
-					IEditorPart editorPartToActivate = editors[i].getEditor(true);
-					EditorSite site = (EditorSite) editorPartToActivate.getEditorSite();
-					
-					if (editorToActivate.equals(site.getPane())) {
-						activePage.activate(editorPartToActivate);
-						return;
-					}
-				}
+			EditorManager editorManager = new EditorManager(getWindow());
+			
+			int activeEditorIndex = editorManager.getIndexForTitle(activeEditor.getTitle());
+			int indexToActivate = getIndexToActivate(activeEditorIndex, editorManager.getEditorParts());
+			IEditorReference editorReference = editorManager.getEditorReferenceForIndex(indexToActivate);
+			if (editorReference != null) {
+				IEditorPart editor = editorReference.getEditor(true);
+				activePage.activate(editor);
 			}
 		}
 	}
 	
-	private EditorPane getEditorToActivate(IEditorPart activeEditor) throws Exception {
-		EditorSite site = (EditorSite) activeEditor.getSite();
-		EditorPane pane = (EditorPane) site.getPane();
-
-		CTabFolder folder = (CTabFolder) pane.getWorkbook().getControl();
-		int selectedIndex = folder.getSelectionIndex();
-		CTabItem[] items = folder.getItems();
-		
-		if (selectedIndex < 0) {
-			return null;
-		}
-		
-		int indexToActivate = getIndexToActivate(selectedIndex, items);
-		setFocusIfTransition(indexToActivate, items);
-		CTabItem item = folder.getItem(indexToActivate);
-		return getEditorPane(item);
-	}
-	
-	protected EditorPane getEditorPane(CTabItem item) throws Exception {
-		DefaultTabItem tabItem = (DefaultTabItem) item.getData();
-		PresentablePart presentable = (PresentablePart) tabItem.getData();
-		return (EditorPane) PluginSupport.getInaccessibleValue(presentable, "part");
-	}
 }
--- devtools/src/com/pfs/devtools/actions/ToggleConsoleAction.java
+++ devtools/src/com/pfs/devtools/actions/ToggleConsoleAction.java
@@ -4,30 +4,55 @@
  */
 package com.pfs.devtools.actions;
 
+import org.eclipse.jface.action.IAction;
 import org.eclipse.ui.IViewPart;
 import org.eclipse.ui.IViewReference;
 import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchPartReference;
 import org.eclipse.ui.console.IConsoleConstants;
 import org.eclipse.ui.internal.WorkbenchPage;
+import org.eclipse.ui.presentations.IStackPresentationSite;
 
-public abstract class ToggleConsoleAction extends ToggleViewAction {
+public abstract class ToggleConsoleAction extends DevToolsAction {
 	public static class Minimize extends ToggleConsoleAction {
 
 		@Override
-		protected void toggleView(WorkbenchPage page, IViewPart view, IViewReference viewRef) {
-			toggleMinimize(page, view, viewRef);
+		protected int getToggleState() {
+			return IStackPresentationSite.STATE_MINIMIZED;
 		}
 	}
 
 	public static class Maximize extends ToggleConsoleAction {
 
 		@Override
-		protected void toggleView(WorkbenchPage page, IViewPart view, IViewReference viewRef) {
-			toggleMaximize(page, view, viewRef);
+		protected int getToggleState() {
+			return IStackPresentationSite.STATE_MAXIMIZED;
 		}
 	}
 
-	protected IViewReference getViewReference(IWorkbenchPage activePage) {
+	
+	protected abstract int getToggleState();
+	
+	private IViewReference getConsoleReference(IWorkbenchPage activePage) {
 		return activePage.findViewReference(IConsoleConstants.ID_CONSOLE_VIEW);
+	}
+	
+	protected void onRun(IAction action) throws Exception {
+		IWorkbenchPage activePage = getWindow().getActivePage();
+		IViewReference activeViewRef = getConsoleReference(activePage);
+
+		if (activeViewRef != null) {
+			toggleView((WorkbenchPage) activePage, activeViewRef);
+		}
+	}
+	
+	private void toggleView(WorkbenchPage page, IViewReference viewRef) {
+		int toggleState = getToggleState();
+		int currentState = page.getPartState(viewRef);
+		if (currentState != IStackPresentationSite.STATE_RESTORED) {
+			page.setPartState(viewRef, IStackPresentationSite.STATE_RESTORED);
+		} else {
+			page.setPartState(viewRef, toggleState);
+		}
 	}
 }
