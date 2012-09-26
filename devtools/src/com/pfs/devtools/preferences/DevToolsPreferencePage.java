515c2a7b686ec2a0b6ed3f1ca3b65ed4d59d04a2
Mike Lueders <mike@roadkill.org>
2012-09-26 18:06:29 -0500

    Added button to prefs page which will set my default prefs... to be used on initial install

--- devtools/src/com/pfs/devtools/preferences/DevToolsPreferencePage.java
+++ devtools/src/com/pfs/devtools/preferences/DevToolsPreferencePage.java
@@ -1,20 +1,14 @@
 package com.pfs.devtools.preferences;
 
-import org.eclipse.core.resources.IWorkspaceDescription;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
-import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.preference.StringButtonFieldEditor;
 import org.eclipse.jface.preference.StringFieldEditor;
-import org.eclipse.jface.util.IPropertyChangeListener;
-import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.eclipse.ui.preferences.ScopedPreferenceStore;
 
-import com.pfs.base.BasePlugin;
 import com.pfs.devtools.DevToolsPlugin;
 
 public class DevToolsPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
@@ -22,7 +16,7 @@ public class DevToolsPreferencePage extends FieldEditorPreferencePage implements
 	private static final class InitWorkspaceDefaults extends StringButtonFieldEditor {
 
 		public InitWorkspaceDefaults(Composite parent) {
-			super("com.pfs.resetdefaults", "Reset default preferences", parent);
+			super("com.pfs.applymydefaultprefs", "Apply My Default Prefs", parent);
 		}
 
 		@Override
@@ -40,6 +34,18 @@ public class DevToolsPreferencePage extends FieldEditorPreferencePage implements
 			setPreference("org.eclipse.ui.editors", "lineNumberRuler", true);
 			setPreference("org.eclipse.ui.ide", "EXIT_PROMPT_ON_CLOSE_LAST_WINDOW", false);
 			setPreference("org.eclipse.ui.ide", "SAVE_ALL_BEFORE_BUILD", true);
+			setPreference("org.eclipse.jdt.ui", "smart_semicolon", true);
+			setPreference("org.eclipse.jdt.ui", "smart_opening_brace", true);
+			setPreference("org.eclipse.debug.ui", "Console.lowWaterMark", 500000);
+			setPreference("org.eclipse.debug.ui", "Console.highWaterMark", 508000);
+			/*
+			String osName = System.getProperty("os.name");
+			if (osName.toLowerCase().contains("mac")) {
+				setPreference("org.eclipse.ui", "KEY_CONFIGURATION_ID", "com.pfs.keybindings.luedersMacAcceleratorConfiguration");
+			} else {
+				setPreference("org.eclipse.ui", "KEY_CONFIGURATION_ID", "com.pfs.keybindings.luedersAcceleratorConfiguration");
+			}
+			*/
 		}
 		
 		private void setPreference(String qualifier, String name, boolean value) throws Exception {
@@ -48,6 +54,18 @@ public class DevToolsPreferencePage extends FieldEditorPreferencePage implements
 	        ps.save();
 		}
 	
+		private void setPreference(String qualifier, String name, int value) throws Exception {
+	        ScopedPreferenceStore ps = new ScopedPreferenceStore(InstanceScope.INSTANCE, qualifier);
+	        ps.setValue(name, value);
+	        ps.save();
+		}
+		
+		private void setPreference(String qualifier, String name, String value) throws Exception {
+	        ScopedPreferenceStore ps = new ScopedPreferenceStore(InstanceScope.INSTANCE, qualifier);
+	        ps.setValue(name, value);
+	        ps.save();
+		}
+		
 	}
 
 	public DevToolsPreferencePage() {
